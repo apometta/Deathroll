d97b93ec7d817f07a5d24c08c8e8b8d34ddacbc9
Andrew H. Pometta <apomet@gmail.com>
2020-04-13 16:23:28 -0700

    Added __sig_p private function, base of other helper functions.

--- DeathrollCalc.py
+++ DeathrollCalc.py
@@ -1,20 +1,20 @@
-"""This file, written by Andrew H. Pometta is the file for calculating and 
-exporting the probability that the first player is going to win a game of 
-Deathrolling given a starting roll of an n-sided die.  As opposed to 
-DeathrollSim.py and DRSimulate.py, this file does not actualy simulate a game 
-of deathrolling - rather, it mathematically calculates the exact probability 
+"""This file, written by Andrew H. Pometta is the file for calculating and
+exporting the probability that the first player is going to win a game of
+Deathrolling given a starting roll of an n-sided die.  As opposed to
+DeathrollSim.py and DRSimulate.py, this file does not actualy simulate a game
+of deathrolling - rather, it mathematically calculates the exact probability
 (or as close to the exact as your computer, OS and Python 3 can get).
 
-The details of deriving the math formula will be relegated to another 
-document.  The math formula requires the use of dynamic programming, so 
-the exported data will necessarily be a range of data, containing the 
-probability of the first player winning for all n in the range [1, N] where 
+The details of deriving the math formula will be relegated to another
+document.  The math formula requires the use of dynamic programming, so
+the exported data will necessarily be a range of data, containing the
+probability of the first player winning for all n in the range [1, N] where
 N is the user's inputted maximum n.
 
 For reference, the formula for all positive integer n > 1 is:
 
-P(n) = c_n(n/[(n^2)-1]) where P(1) = 1 and 
-c(n) = 1 + sum[i=2 to n-1](1/i(sigma[j=2 to i](P(j)))) + 
+P(n) = c_n(n/[(n^2)-1]) where P(1) = 1 and
+c(n) = 1 + sum[i=2 to n-1](1/i(sigma[j=2 to i](P(j)))) +
       1/n(sigma[j=2 to n-1](P(j)))
 Alternatively, c can be defined recursively as such:
 c(n) = c(n-1) + (1/(n-1))sigma[j=2 to n-1](P(j)) + (1/n)P(n-1) where c(2) = 1
@@ -22,10 +22,10 @@ c(n) = c(n-1) + (1/(n-1))sigma[j=2 to n-1](P(j)) + (1/n)P(n-1) where c(2) = 1
 
 Sorry if my notation sucks.
 
-The strategy to calculate the values effeciently quite obviously utilizes 
-dynamic programming, seeing as P(n) is a recursive piecewise function, to 
-which P(k) for any k requires knowledge of all P(l) for l in [1, k-1].  We 
-use one list to keep track of the running data for P(n) for quick reference, 
+The strategy to calculate the values effeciently quite obviously utilizes
+dynamic programming, seeing as P(n) is a recursive piecewise function, to
+which P(k) for any k requires knowledge of all P(l) for l in [1, k-1].  We
+use one list to keep track of the running data for P(n) for quick reference,
 and another few variables to keep track of c(n) as n progresses.
 """
 
@@ -46,9 +46,9 @@ __p_n = np.array([1], dtype=float)
 # we can define it properly and just never reference the 0th index
 __c_n = np.array([1, 1], dtype=float)
 
-# This array represents, for each index i, the sum off all P(k) for which k is in
-# the set [2, k-1].  Used for more efficienctly calculating c(n), avoiding an
-# O(n) procedure for each calculation
+# This array represents, for each index i, the sum off all P(k) for which k is
+# in the set [2, k-1].  Used for more efficienctly calculating c(n), avoiding
+# an O(n) procedure for each calculation
 __sig_p_n = np.array([0, 0], dtype=float)
 
 """Custom exception class for ValueError."""
@@ -71,3 +71,39 @@ def __posint(arg, param):
         raise DeathrollCalcValueError(
             "Argument {} for {} is not positive".format(arg, param))
     return arg
+
+
+"""Function for either fetching or, if not previously requested, calculating 
+the sum of all P(k) in the range [2, k-1]."""
+
+
+def __sig_p(n):
+    n = __posint(n, "n")
+    global __sig_p_n
+    try:
+        return __sig_p_n[n - 1]
+    except IndexError:
+        # We haven't done this yet, so we calculate it
+        # this is the manual way:
+        # assert len(__p_n) <= n
+        # total = np.sum(__p_n[1:n])
+        # the recursive way
+        total = __sig_p(n - 1) + __p_n(n - 1)
+        __sig_p_n = np.append(__sig_p_n, total)  # update cache
+        return total
+
+
+"""Function for either fetching or, if not previously requested, calculating 
+P(n)."""
+
+
+def __p(n):
+    pass
+
+
+"""Function for either fetching or, if not previously requested, calculating 
+c(n)."""
+
+
+def __c(n):
+    pass
