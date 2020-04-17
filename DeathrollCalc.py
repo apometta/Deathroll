9f697178b2cd6831d054233dc32dfc8491895caf
Andrew H. Pometta <apomet@gmail.com>
2020-04-16 18:02:08 -0700

    Added more functions, but will remove range functions soon.

--- DeathrollCalc.py
+++ DeathrollCalc.py
@@ -6,10 +6,10 @@ of deathrolling - rather, it mathematically calculates the exact probability
 (or as close to the exact as your computer, OS and Python 3 can get).
 
 The details of deriving the math formulas will be relegated to another
-document.  Since our solution involves dynamic programming, we will 
+document.  Since our solution involves dynamic programming, we will
 continually keep track of a summation as our calculations progress.
 
-For finding the expected number of rolls per game, a nearly identical 
+For finding the expected number of rolls per game, a nearly identical
 approach is used, but a small difference in the formula.
 """
 
@@ -50,20 +50,20 @@ class DeathrollCalcValueError(ValueError):
 """Local private function for testing if a number is a positive integer."""
 
 
-def __posint(arg):
+def __posint(arg, param="n"):
     try:
         arg = int(arg)
     except ValueError:
-        raise DeathrollCalcValueError("Argument {} for n cannot be cast "
-                                      "as an integer".format(arg))
+        raise DeathrollCalcValueError("Argument {} for {} cannot be cast "
+                                      "as an integer".format(arg, param))
     if arg < 1:
         raise DeathrollCalcValueError(
-            "Argument {} for n is not positive".format(arg))
+            "Argument {} for {} is not positive".format(arg, param))
     return arg
 
 
-"""Function for either fetching or, if not previously requested, calculating 
-the sum of all P_w1(k) in the range [2, k].  Note that P_w1(n) is just 
+"""Function for either fetching or, if not previously requested, calculating
+the sum of all P_w1(k) in the range [2, k].  Note that P_w1(n) is just
 1 - P_l1(n)."""
 
 
@@ -79,7 +79,7 @@ def __sig_p_w1(n):
         return total
 
 
-"""Function for either fetching or, if not previously requested, calculating 
+"""Function for either fetching or, if not previously requested, calculating
 P_l1(n)."""
 
 
@@ -94,7 +94,7 @@ def __p_l1(n):
         return total
 
 
-"""Function for either fetching or, if not previously requested, caluclating 
+"""Function for either fetching or, if not previously requested, caluclating
 the sum of all R(k) in the range [2, k]."""
 
 
@@ -109,7 +109,7 @@ def __sig_r(n):
         return total
 
 
-"""Function for either fetching or, if not previously requested, calculating 
+"""Function for either fetching or, if not previously requested, calculating
 R(n)."""
 
 
@@ -123,16 +123,65 @@ def __r(n):
         __r_n = np.append(__r_n, total)
         return total
 
-"""User-accessible functions begin here.  They are mostly wrappers around the above 
-functions in one way or another."""
+"""User-accessible functions begin here.  They are mostly wrappers around the
+above functions in one way or another."""
 
-"""Function for getting P_w1(n) for a single value of n.  Raises a 
+"""Function for getting P_w1(n) for a single value of n.  Raises a
 DeathrollCalcValueError if n is not positive, or not castable as an integer."""
 
 
-def p_w1(n):
+def p1_winrate(n):
     return 1 - __p_l1(n)
 
 
-"""Function for getting a range of values for P, in the range [m, n).  Raises 
-DeathrolLCalcValueError if either would be invalid arguments for p_w1()."""
+"""Function for getting P_w2(n) for a single value of n.  Raises a
+DeathrollCalcValueError if n is not positive, or not castable as an integer."""
+
+
+def p2_winrate(n):
+    return __p_l1(n)
+
+
+"""Function for getting a range of values for P, in the range [m, n) with a
+difference between each integer by value step.  Default step of 1, or negative
+1 if m > n.  Default m of 1 if only one argument supplied.  Raises a
+DeathrollCalcValueError should any of the arguments not be castable as an
+integer, or if either m or n are not positive."""
+
+
+def p1_winrate_range(m, n=None, step=1):
+    if n == None:
+        m, n = 1, m
+    if n != 0:
+        # manually skip checking for 0, since it's allowed for n (not for m)
+        n = __posint(n, "n")
+    m = __posint(m, "m")
+    try:
+        step = int(step)
+    except ValueError:
+        raise DeathrollCalcValueError(
+            "Argument {} for step not castable as an integer".format(step))
+    __p_l1(max(m, n))  # ensure that all relevant values are calculated
+    if m > n and step == 1:
+        step = -1
+
+    print(m, n, step)
+    # 0 is a valid input for n, but we have to do it this way or else the
+    # slicing doesn't work right
+    data = __p_l1_n[m - 1: n - 1: step] if n > 0 else __p_l1_n[m - 1::step]
+    return 1 - data
+
+
+"""Same as p1_winrate_range, but for player 2's winrate."""
+
+
+def p2_winrate_range(m, n=None, step=1):
+    return 1 - p1_winrate_range(m, n, step)
+
+"""Function for getting R(n) at a given value.   Returns
+DeathrollCalcValueError if n is not positive, or castable as an integer."""
+
+
+def avg_rolls(n):
+    n = __posint(n)
+    return __r(n)
