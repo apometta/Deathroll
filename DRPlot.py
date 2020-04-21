5828e00e75698a70de9f271d2dd4ef1135c100cb
Andrew H. Pometta <apomet@gmail.com>
2020-04-20 18:01:00 -0700

    Adding DRPlot with initial comment.

---
+++ DRPlot.py
@@ -0,0 +1,6 @@
+"""This file, written by Andrew H. Pometta, is the main file for the Deathroll 
+project this file is apart of.  It uses both DRSimulate.py and 
+DeathrollCalc.py to get data on the winrates and average roll counts in 
+Deathroll games of different starting conditions, as a guide for players that 
+wish to know the expected results of a Deathroll.  It uses this data to 
+generate graphs, to be used in a larger report."""
