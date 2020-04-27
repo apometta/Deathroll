5f060a4ce4eb05561fd7d9c11dae9f44699830aa
Andrew H. Pometta <apomet@gmail.com>
2020-04-27 16:16:34 -0700

    Initial commit, added function for inputting test data.

--- DRPlot.py
+++ DRPlot.py
@@ -4,3 +4,19 @@ DeathrollCalc.py to get data on the winrates and average roll counts in
 Deathroll games of different starting conditions, as a guide for players that 
 wish to know the expected results of a Deathroll.  It uses this data to 
 generate graphs, to be used in a larger report."""
+
+"""A temporary holder function to generate random data in the vague form of 
+what my actual data might be.  One of these is the experimental data, the 
+other is mathed."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def __rdata():
+    exp = np.array([1, 2 / 3, 0.62, 0.59])
+    for i in range(96):
+        diff_exp = exp[len(exp) - 1] - 0.5
+        exp = np.append(exp, np.random.random() * diff_exp + 0.5)
+
+    return exp
