c22334e3c7468db65b5f6d2c8e262f67df3c1b81
Andrew H. Pometta <apomet@gmail.com>
2020-05-04 17:52:29 -0700

    Adjusted annotation positions, made each figure optional.

--- DRPlot.py
+++ DRPlot.py
@@ -24,7 +24,11 @@ calc_max = 1000
 # domain for monte carlo
 mc_range = [2, 5, 10, 25, 50, 100, 500, 1000]
 # sample size for the Monte Carlo simuation
-mc_samples = 10_000
+mc_samples = 5_000
+# whether or not to even graph the winrate graph
+wr_graph = True
+# and likewise for the rolls
+rolls_graph = True
 
 """Settings for the graph of winrates."""
 
@@ -83,123 +87,132 @@ p1_winrate_mc = mc_data[:, 0]
 p2_winrate_mc = 1 - p1_winrate_mc
 avg_rolls_mc = mc_data[:, 1]
 
-p1_winrate_calc = drc.p1_winrate(calc_range)
-# Since DeathrollCalc saves the values stored thus far, this isn't double the
-# time spent
-p2_winrate_calc = drc.p2_winrate(calc_range)
-avg_rolls_calc = drc.avg_rolls(calc_range)
-if rolls_log:  # the range of ln, from 1 to 1000
-    log_range = np.fromfunction(np.vectorize(lambda n: math.log(n + 1)),
-                                (calc_max,))
+if wr_graph:
+    # DeathrollCalc stores the information calculated thus far, meaning we
+    # aren't reduplicating efforts in getting data twice
+    p1_winrate_calc = drc.p1_winrate(calc_range)
+    p2_winrate_calc = drc.p2_winrate(calc_range)
+if rolls_graph:
+    avg_rolls_calc = drc.avg_rolls(calc_range)
+    if rolls_log:  # the range of ln, from 1 to 1000
+        log_range = np.fromfunction(np.vectorize(lambda n: math.log(n + 1)),
+                                    (calc_max,))
 
 """Create the figure for the winrates."""
 
 # set up the figure for winrates
-winrate_fig = plt.figure(1, facecolor="#CCCCCC", figsize=graph_size,
-                         dpi=resolution)
-winrate_fig.canvas.set_window_title("Deathroll Win Probability")
-# setting the properties of the axes
-winrates = plt.subplot(111, facecolor="#EEEEEE", xlabel="/roll Value",
-                       ylabel="Probability of winning")
-# set the x axis as a log scale if necessary, but keep the tick values scalars
-if wr_logx:
-    from matplotlib.ticker import ScalarFormatter
-    plt.xscale("log", basex=wr_logbase)
-    winrates.xaxis.set_major_formatter(ScalarFormatter())
-# set percentages for y axis if necessary
-if wr_pery:
-    from matplotlib.ticker import PercentFormatter
-    winrates.yaxis.set_major_formatter(PercentFormatter(xmax=1))
+if wr_graph:
+    winrate_fig = plt.figure(1, facecolor="#CCCCCC", figsize=graph_size,
+                             dpi=resolution)
+    winrate_fig.canvas.set_window_title("Deathroll Win Probability")
+    # setting the properties of the axes
+    winrates = plt.subplot(111, facecolor="#EEEEEE", xlabel="/roll Value",
+                           ylabel="Probability of winning")
+    # set the x axis as a log scale if necessary, but keep the tick values
+    # scalars
+    if wr_logx:
+        from matplotlib.ticker import ScalarFormatter
+        plt.xscale("log", basex=wr_logbase)
+        winrates.xaxis.set_major_formatter(ScalarFormatter())
+    # set percentages for y axis if necessary
+    if wr_pery:
+        from matplotlib.ticker import PercentFormatter
+        winrates.yaxis.set_major_formatter(PercentFormatter(xmax=1))
 
-"""Perform the actual data plotting.  The data from [2, 1000] is plotted
-first with one alpha, for both the calculations and the Monte Carlo data.  The
-legend is then displayed, and the data for [1, 2] is only plotted after this,
-so it doesn't appear on the legend.  It uses a different alpha value."""
+    """Perform the actual data plotting.  The data from [2, 1000] is plotted
+	first with one alpha, for both the calculations and the Monte Carlo data.  
+	The legend is then displayed, and the data for [1, 2] is only plotted 
+	after this, so it doesn't appear on the legend.  It uses a different alpha 
+	value."""
 
-plt.plot(calc_range[1:], p1_winrate_calc[1:], "-", color="red",
-         alpha=wr_alpha, label="Player 1 Winrate (Exact Formula)")
-plt.plot(calc_range[1:], p2_winrate_calc[1:], "-", color="blue",
-         alpha=wr_alpha, label="Player 2 Winrate (Exact Formula)")
-plt.plot(mc_range, p1_winrate_mc, "x", color="darkred", alpha=wr_alpha,
-         label="Player 1 Winrate (Monte Carlo)")
-plt.plot(mc_range, p2_winrate_mc, "x", color="darkblue", alpha=wr_alpha,
-         label="Player 2 Winrate (Monte Carlo)")
-winrates.legend(loc="lower right")
-plt.plot(calc_range[:2], p1_winrate_calc[:2], "-", color="red",
-         alpha=wr_first_alpha)
-plt.plot(calc_range[:2], p2_winrate_calc[:2], "-", color="blue",
-         alpha=wr_first_alpha)
+    plt.plot(calc_range[1:], p1_winrate_calc[1:], "-", color="red",
+             alpha=wr_alpha, label="Player 1 Winrate (Exact Formula)")
+    plt.plot(calc_range[1:], p2_winrate_calc[1:], "-", color="blue",
+             alpha=wr_alpha, label="Player 2 Winrate (Exact Formula)")
+    plt.plot(mc_range, p1_winrate_mc, "x", color="darkred", alpha=wr_alpha,
+             label="Player 1 Winrate (Monte Carlo)")
+    plt.plot(mc_range, p2_winrate_mc, "x", color="darkblue", alpha=wr_alpha,
+             label="Player 2 Winrate (Monte Carlo)")
+    winrates.legend(loc="lower right")
+    plt.plot(calc_range[:2], p1_winrate_calc[:2], "-", color="red",
+             alpha=wr_first_alpha)
+    plt.plot(calc_range[:2], p2_winrate_calc[:2], "-", color="blue",
+             alpha=wr_first_alpha)
 
-# set the xticks and yticks
-plt.xticks(wr_xticks)
-# unfortunately setting the x axis tick labels is harder than it seems when
-# you want it particular.
-ticklabels = []
-for i in wr_xticks:
-    if i in wr_xlabels:
-        ticklabels.append(i)
-    else:
-        ticklabels.append('')
-winrates.xaxis.set_ticklabels(ticklabels)
-plt.yticks(np.arange(0, 1.1, 0.1))
+    # set the xticks and yticks
+    plt.xticks(wr_xticks)
+    # unfortunately setting the x axis tick labels is harder than it seems when
+    # you want it particular.
+    ticklabels = []
+    for i in wr_xticks:
+        if i in wr_xlabels:
+            ticklabels.append(i)
+        else:
+            ticklabels.append('')
+    winrates.xaxis.set_ticklabels(ticklabels)
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    # and set the background grid
+    plt.grid(axis='y', alpha=0.1)
 
-"""Perform wr graph annotations."""
+    """Perform wr graph annotations."""
 
-if wr_annotate:
-    annotate("For a 2 sided-die, the smallest\npossible deathroll, "
-             "the current\nroller only has a 33.3% chance \nof winning.",
-             2, p1_winrate_calc, (1.4, 0))
-    annotate("When the die has 5 sides, the \ngap in winrate has already "
-             "substantially\ndecreased: the current roller has \na 46.6% "
-             "chance at winning.", 5, p1_winrate_calc, (3, 0.175))
-    annotate("By /roll 10, this probability \nrises barely above 49%.",
-             10, p1_winrate_calc, (6, 0.37))
-    annotate("At /roll 25, the difference in \nwinrates is less than 0.5%.",
-             25, p2_winrate_calc, (14, 0.55))
-    annotate("For a deathroll of 100, \nthe difference in winrates is "
-             "\nless then 0.02%.", 100, p1_winrate_calc, (65, 0.40))
-    annotate("At /roll 1000, the gap \nbetween winrates is \ninfinitesimal.",
-             1000, p2_winrate_calc, (625, 0.55))
+    if wr_annotate:
+        annotate("For a 2 sided-die, the smallest\npossible deathroll, "
+                 "the current\nroller only has a 33.3% chance \nof winning.",
+                 2, p1_winrate_calc, (1.4, 0))
+        annotate("When the die has 5 sides, the \ngap in winrate has already "
+                 "substantially\ndecreased: the current roller has \na 46.6% "
+                 "chance at winning.", 5, p1_winrate_calc, (3, 0.175))
+        annotate("By /roll 10, this probability \nrises barely above 49%.",
+                 10, p1_winrate_calc, (7, 0.33))
+        annotate("At /roll 25, the difference in \nwinrates is less than "
+                 "0.5%.", 25, p2_winrate_calc, (16.2, 0.41))
+        annotate("For a deathroll of 100, \nthe difference in winrates is "
+                 "\nless then 0.02%.", 100, p1_winrate_calc, (65, 0.41))
+        annotate("At /roll 1000, the gap \nbetween winrates is "
+                 "\ninfinitesimal.", 1000, p2_winrate_calc, (625, 0.42))
+
+
 """Set up the rolls graph."""
 
-rolls_fig = plt.figure(2, facecolor="#CCCCCC", figsize=graph_size,
-                       dpi=resolution)
-rolls_fig.canvas.set_window_title("Average Rolls per Deathroll")
-# the axes for rolls is a bit simpler
-rolls = plt.subplot(111, facecolor="#EEEEEE", xlabel="/roll Value",
-                    ylabel="Average Rolls per Game")
-# set x axis scale
-if rolls_logx:
-    # Since the graph is to be viewed by the less mathematically inclined,
-    # scientific notation should be eschewed
-    from matplotlib.ticker import ScalarFormatter
-    plt.xscale("log", basex=rolls_logbase)
-    rolls.xaxis.set_major_formatter(ScalarFormatter())
-# set y maximum
-if rolls_ymax > 0:
-    plt.ylim(0, rolls_ymax)
+if rolls_graph:
+    rolls_fig = plt.figure(2, facecolor="#CCCCCC", figsize=graph_size,
+                           dpi=resolution)
+    rolls_fig.canvas.set_window_title("Average Rolls per Deathroll")
+    # the axes for rolls is a bit simpler
+    rolls = plt.subplot(111, facecolor="#EEEEEE", xlabel="/roll Value",
+                        ylabel="Average Rolls per Game")
+    # set x axis scale
+    if rolls_logx:
+            # Since the graph is to be viewed by the less mathematically
+            # inclined, scientific notation should be eschewed
+        from matplotlib.ticker import ScalarFormatter
+        plt.xscale("log", basex=rolls_logbase)
+        rolls.xaxis.set_major_formatter(ScalarFormatter())
+    # set y maximum
+    if rolls_ymax > 0:
+        plt.ylim(0, rolls_ymax)
 
+    """Plot the data for the rolls."""
 
-"""Plot the data for the rolls."""
-
-plt.plot(calc_range[1:], avg_rolls_calc[1:], "-", color="green",
-         alpha=rolls_alpha, label="Average Rolls per Game (Exact Formula)")
-plt.plot(mc_range, avg_rolls_mc, "x", color="darkgreen", alpha=rolls_alpha,
-         label="Average Rolls per Game (Monte Carlo)")
-# Since the functions converge to each other as x approaches infinite, it
-# might be interesting to compare them
-if rolls_log:
-    plt.plot(calc_range[1:], log_range[1:], "-", color="orange",
-             alpha=rolls_alpha, label="log2")
-rolls.legend(loc="lower right")
-plt.plot(calc_range[:2], avg_rolls_calc[:2], "-", color="green",
-         alpha=rolls_first_alpha)
-if rolls_log:
-    plt.plot(calc_range[:2], log_range[:2], "-", color="orange",
+    plt.plot(calc_range[1:], avg_rolls_calc[1:], "-", color="green",
+             alpha=rolls_alpha, label="Average Rolls per Game (Exact Formula)")
+    plt.plot(mc_range, avg_rolls_mc, "x", color="darkgreen", alpha=rolls_alpha,
+             label="Average Rolls per Game (Monte Carlo)")
+    # Since the functions converge to each other as x approaches infinite, it
+    # might be interesting to compare them
+    if rolls_log:
+        plt.plot(calc_range[1:], log_range[1:], "-", color="orange",
+                 alpha=rolls_alpha, label="log2")
+    rolls.legend(loc="lower right")
+    plt.plot(calc_range[:2], avg_rolls_calc[:2], "-", color="green",
              alpha=rolls_first_alpha)
+    if rolls_log:
+        plt.plot(calc_range[:2], log_range[:2], "-", color="orange",
+                 alpha=rolls_first_alpha)
 
-# set the ticks to be one at a time based on current ylim
-plt.yticks(np.arange(0, plt.ylim()[1] + 1, 1))
+    # set the ticks to be one at a time based on current ylim
+    plt.yticks(np.arange(0, plt.ylim()[1] + 1, 1))
 
 # finally, show the graph
 plt.show()
